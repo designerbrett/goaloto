1e6b246cb90ccda636e859fee446ccf6b939a3fe
designerbrett <brettdesigner@gmail.com>
2024-01-04 22:16:10 -0800

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -6,6 +6,8 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9345857399419477"
+     crossorigin="anonymous"></script>
     </Head>
   );
 }
