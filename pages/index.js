e7a3959e544f989213aaada9753a65d4b29271d9
designerbrett <brettdesigner@gmail.com>
2024-01-04 20:54:24 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Index({ posts, globalData }) {
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
         </h1>
+        <h2>Custom title below</h2>
         <ul className="w-full">
           {posts.map((post) => (
             <li
